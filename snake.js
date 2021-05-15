be81322bb733467a89676a32ae61233229347653
SalomeKelaptrishvili <83080867+SalomeKelaptrishvili@users.noreply.github.com>
2021-05-15 19:06:38 +0400

    Add files via upload

--- snake.js
+++ snake.js
@@ -71,21 +71,41 @@ const snakeBoard = document.getElementById("frame");
 //     die();
 // }
 
-setInterval(game,300);
+// setInterval(game,200);
+setInterval(()=>{
+    game()
+},200);
+
+
+
+let gameOver = false;
 
 function game(){
-    move();
-    createApple(snakeBoard);
-}
-
-function move(){
+    // if(gameOver) {
+    //     snakeBoard.innerHTML = "";
+    //     alert("game over");
+    //     return;
+    // }
     moveSnakeSquare();
-    snakeBoard.innerHTML="";
+    snakeBoard.innerHTML = "";
     createSnakeSquare(snakeBoard);
+
+    updateFood();
+    createApple(snakeBoard);
+    death();
 }
 
-let snakeBody = [{x: 10, y:11}]; //initially snake has only 1 square
-let initialDirectionChange = {x: 0, y: 0};
+
+
+let snakeBody = [{x: 3, y:15},
+                {x: 4, y:15},
+                {x: 5, y:15},
+                {x: 6, y:15}
+               ]; //initially snake has only 1 square
+            //    {x: 11, y:11},
+            //    {x: 12, y:11},
+            //    {x: 13, y:11}
+let initialDirectionChange = {x: 1, y: 0};
 let prevDirectionChange = {x: 0, y: 0};
 
 function directionChange(){
@@ -93,11 +113,19 @@ function directionChange(){
     return initialDirectionChange;
 }
 function moveSnakeSquare(){
-    for(let i = snakeBody.length - 1; i > 0; i--){
-        snakeBody[i] = snakeBody[i] - 1;
+    addSegments();
+    let inputD = directionChange();
+    for(let i = snakeBody.length - 2; i >= 0; i--){
+        
+        snakeBody[i + 1] = {...snakeBody[i]};
     }
-    snakeBody[0].x += directionChange().x;
-    snakeBody[0].y += directionChange().y;
+
+    // snakeBody[0].x += directionChange().x;
+    // snakeBody[0].y += directionChange().y;
+    // snakeBody[0].x += 0;
+    // snakeBody[0].y += 1;
+    snakeBody[0].x += inputD.x;
+    snakeBody[0].y += inputD.y;
 }
 
 function createSnakeSquare(snakeBoard){
@@ -110,21 +138,65 @@ function createSnakeSquare(snakeBoard){
         snakeSquare.style.gridColumnStart = square.x;
     })
 }
+let food = randomApple(); //{x:15, y:12};
+let newSegments = 0;
 
-let appleInitialPosition = {x:15, y:2};
+function appleCheck(position){
+    // snakeBody.forEach(function(square){
+    //     if(square.x == position.x && square.y == position.y){
+    //         eatApple(1);
+    //     }
+    // })
+    // return snakeBody.some(square =>{
+    //     return  (square.x == position.x && square.y == position.y);
+    // })
+    let res = false;
+    snakeBody.forEach(function(square){
+        if(square.x == position.x && square.y == position.y){
+            res =  true;
+        }
+    })
+    return res;
+}
 
+function eatApple(amount){
+    newSegments += amount;
+}
+
+function updateFood(){
+    // appleCheck(food);
+    // food = {x: 4, y:4};
+    if(appleCheck(food)){
+        eatApple(3);
+        food = randomApple(); //{x: 4, y:4};
+    }
+}
+
+// let appleInitialPosition = {x:15, y:2};
 function createApple(snakeBoard){
         let apple = document.createElement('div');
         apple.classList.add('apple');
         snakeBoard.appendChild(apple);
 
-        apple.style.gridRowStart = appleInitialPosition.y;
-        apple.style.gridColumnStart = appleInitialPosition.x;
+        apple.style.gridRowStart = food.y;
+        apple.style.gridColumnStart = food.x;
 }
 
-//keyboard listeners
+// let appleRandomPosition = {x: 2, y: 8};
+function randomApple(){
+    let appleRandomPosition = {x: 0, y: 0};
+   // while(appleCheck(appleRandomPosition)){
+        appleRandomPosition.x = Math.floor(Math.random() * 20) + 1;
+        appleRandomPosition.y = Math.floor(Math.random() * 20) + 1;
+    //}
+    return appleRandomPosition;
+}
+
+snakeInitDirection = 37;
+// //keyboard listeners
 document.addEventListener('keydown',function(event){
     //left arrow
+
     if(event.keyCode === 37 && prevDirectionChange.x === 0){
       initialDirectionChange = {x: -1, y: 0};
   }
@@ -142,13 +214,24 @@ document.addEventListener('keydown',function(event){
   }
 })
 
-function randomApple(){
-    let appleRandomPosition;
-    while(appleRandomPosition != null){
-        appleRandomPosition.x = Math.floor(Math.random() * 20) + 1;
-        appleRandomPosition.y = Math.floor(Math.random() * 20) + 1;
+
+
+
+
+
+
+function addSegments(){
+    for(let i = 0; i < newSegments; i++){
+        snakeBody.push({... snakeBody[snakeBody.length - 1]});
     }
-    return appleRandomPosition;
+    newSegments = 0;
+}
+function death(){
+    //check if snake bumps into itself
+    for(let i = 1; i < snakeBody.length; i++){
+        if(snakeBody[0].x == snakeBody[i].x && snakeBody[0].y == snakeBody[i].y) gameOver = true;
+    }
+    return gameOver;
 }
 
 // let snakeSquare = document.getElementById("snakeSquareId");
